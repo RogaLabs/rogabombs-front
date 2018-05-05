c21328051ea2c278b7b0449cdcc0d908c0875b2f
Ataide <ataide.neto31@gmail.com>
2018-05-05 16:51:45 -0300

    add navbar template
    Signed-off-by: Ataide <ataide.neto31@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+                                                                                                       <template>
   <v-app>
     <v-navigation-drawer
       persistent
---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="main-navbar">
+    <li class="item">Painel</li>
+    <li class="item">Jogadors</li>
+    <li class="item">Partidas</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  };
+</script>
+
+<styles lang="sass">
+  .main-navbar {
+    > .item{
+      display: inline-block;
+    }
+  }
+</styles>
