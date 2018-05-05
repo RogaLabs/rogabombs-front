b5b184005c30ecad9e4a444c45a18e24233d531c
Ataide <ataide.neto31@gmail.com>
2018-05-05 17:12:11 -0300

    remove flat duplicated
    Signed-off-by: Ataide <ataide.neto31@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <v-toolbar>
       <v-spacer></v-spacer>
       <v-toolbar-items v-for="screen in screens" :key="screen.id">
-        <v-btn flat route :to="{ path: screen.url }"flat>{{ screen.title }}</v-btn>
+        <v-btn route :to="{ path: screen.url }"flat>{{ screen.title }}</v-btn>
       </v-toolbar-items>
     </v-toolbar>
 
