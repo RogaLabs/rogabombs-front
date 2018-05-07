85a0ec680214d7bbea61b20226cfe850f43639a8
Valter Barros <valterinside@gmail.com>
2018-05-07 00:25:46 -0300

    add-styles-and-customize-date-picker
    - Hide the datepicker and add a p tag to open datepicker and get date from input
    - Add border radius to players table and add correct colors
    
    Signed-off-by: Valter Barros <valterinside@gmail.com>

--- src/components/players/Players.vue
+++ src/components/players/Players.vue
@@ -21,18 +21,20 @@
       </v-layout>
 
       <div class="date-picker">
-        <v-menu
+        <v-menu 
           ref="menu1"
           :close-on-content-click="false"
           v-model="menu1"
           :nudge-right="40"
           color="white"
           lazy
+          style="visibility: collapse; position:absolute"
           transition="scale-transition"
-          offset-y
-        >
-          <v-text-field
+          offset-y>
+          
+          <v-text-field dark
             slot="activator"
+            class="input-datepicker"
             v-model="dateFormatted"
             label="Date"
             hint="MM/DD/YYYY format"
@@ -42,8 +44,13 @@
             @blur="date = parseDate(dateFormatted)"
           ></v-text-field>
 
-          <v-date-picker  v-model="date" no-title @input="menu1 = false"></v-date-picker>
+          <v-date-picker v-model="date" no-title @input="menu1 = false"></v-date-picker>
+          
         </v-menu>
+        <p class="select-date" @click="openDatePicker">
+          {{ selectDateTitle }}
+          <v-icon color="white">chevron_right</v-icon>
+        </p>
       </div>
 
     </v-layout>
@@ -51,7 +58,15 @@
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss">
+.menu__content .menuable__content__active {
+  min-width: 200px;
+  top: 673px;
+  left: 192px;
+  transform-origin: left top 0px;
+  z-index: 8;
+}
+
 .new-player {
   align-self: flex-end;
 }
@@ -59,11 +74,20 @@
 .date-picker {
   max-width: 200px;
   width: 100%;
+  position: relative;
+}
+
+.select-date {
+  color: white;
+  cursor: pointer;
+  position: absolute;
+  top: 13px;
+  left: 0;
 }
 </style>
 
 <script>
-import NewPlayer from './NewPlayer';
+import NewPlayer from "./NewPlayer";
 
 export default {
   components: { NewPlayer },
@@ -74,58 +98,64 @@ export default {
       menu1: false,
       menu2: false,
       headers: [
-        { text: 'Jogador', value: 'name' },
-        { text: 'Partidas Jogadas', value: 'matches' },
-        { text: 'Vitórias', value: 'victories' },
-        { text: 'Ranking', value: 'ranking' },
+        { text: "Jogador", value: "name" },
+        { text: "Partidas Jogadas", value: "matches" },
+        { text: "Vitórias", value: "victories" },
+        { text: "Ranking", value: "ranking" }
       ],
       players: [
         {
-          name: 'Negao do zap',
+          name: "Negao do zap",
           matches: 123,
           victories: 123,
-          ranking: 1,
+          ranking: 1
         },
         {
-          name: 'Atalabs',
+          name: "Atalabs",
           matches: 75,
           victories: 69,
-          ranking: 2,
+          ranking: 2
         },
         {
-          name: 'Mano Ralte',
+          name: "Mano Ralte",
           matches: 66,
           victories: 88,
-          ranking: 3,
-        },
-      ],
+          ranking: 3
+        }
+      ]
     };
   },
   computed: {
     computedDateFormatted() {
       return this.formatDate(this.date);
     },
+    selectDateTitle() {
+      return this.dateFormatted ? this.dateFormatted : "Select a Date";
+    }
   },
 
   watch: {
     date() {
       this.dateFormatted = this.formatDate(this.date);
-    },
+    }
   },
 
   methods: {
     formatDate(date) {
       if (!date) return null;
 
-      const [year, month, day] = date.split('-');
+      const [year, month, day] = date.split("-");
       return `${month}/${day}/${year}`;
     },
     parseDate(date) {
       if (!date) return null;
 
-      const [month, day, year] = date.split('/');
-      return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+      const [month, day, year] = date.split("/");
+      return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
     },
-  },
+    openDatePicker(){
+      this.$el.querySelector('.input-datepicker').click()
+    }
+  }
 };
 </script>
