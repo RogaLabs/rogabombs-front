a2a8caf2be696dc466f05bcb4e4832f50d0bbb78
arthurdenner <arthurdenner7@gmail.com>
2018-05-13 21:11:54 -0300

    Transforming the new user dialog into a form

--- src/components/players/NewPlayer.vue
+++ src/components/players/NewPlayer.vue
@@ -5,34 +5,66 @@
         <v-icon>add</v-icon>
       </v-btn>
       <v-card>
-        <v-card-title>
-          <span class="headline">Novo Jogador</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container grid-list-md>
-            <v-layout wrap>
-              <v-flex xs12>
-                <v-text-field label="Nome ou Apelido" required></v-text-field>
-              </v-flex>
-            </v-layout>
-          </v-container>
-          <small>* Campo obrigatório</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false">Fechar</v-btn>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false">Confirmar</v-btn>
-        </v-card-actions>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-card-title>
+            <span class="headline">Novo Jogador</span>
+          </v-card-title>
+          <v-card-text>
+              <v-text-field
+                v-model="name"
+                :rules="[v => !!v || 'Campo obrigatório!']"
+                label="Nome ou apelido"
+                required
+              ></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              flat
+              @click="clear"
+              :disabled="loading"
+            >
+              Fechar
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              flat
+              :disabled="!valid || loading"
+              :loading="loading"
+              @click="submit"
+            >
+              Enviar
+            </v-btn>
+          </v-card-actions>
+        </v-form>
       </v-card>
     </v-dialog>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'NewPlayer',
-    data: () => ({
-      dialog: false,
-    }),
-  };
+export default {
+  name: 'NewPlayer',
+  data: () => ({
+    dialog: false,
+    loading: false,
+    name: '',
+    valid: true,
+  }),
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) {
+        this.loading = true;
+
+        setTimeout(() => {
+          this.loading = false;
+        }, 3000);
+      }
+    },
+    clear() {
+      this.$refs.form.reset();
+      this.dialog = false;
+    },
+  },
+};
 </script>
